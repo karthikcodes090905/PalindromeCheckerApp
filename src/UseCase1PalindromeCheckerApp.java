02b02381ece47d30d4a9a9b8a8485c3d2d6b710d
karthikcodes090905 <kc5182@srmist.edu.in>
2026-03-10 20:33:21 +0530

    UC2: Hardcoded palindrome check

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -3,18 +3,27 @@ public class UseCase1PalindromeCheckerApp {
     public static void main(String[] args) {
 
         System.out.println("===================================");
-        System.out.println("     PALINDROME CHECKER APP");
+        System.out.println("      PALINDROME CHECKER APP");
         System.out.println("===================================");
 
-        System.out.println("Application Name  : Palindrome Checker");
-        System.out.println("Version           : 1.0");
-        System.out.println("Developer         : Karthik C");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
 
-        System.out.println("\nWelcome to the Palindrome Checker Application!");
-        System.out.println("This program will help you check whether a given string is a palindrome.");
+        // UC2 Hardcoded String
+        String original = "madam";
+        String reversed = "";
 
-        System.out.println("\nProgram started successfully.");
+        for(int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
 
+        System.out.println("Original String : " + original);
+        System.out.println("Reversed String : " + reversed);
+
+        if(original.equals(reversed)) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
+        }
     }
-
 }
