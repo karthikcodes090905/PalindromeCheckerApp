222ace0ffc027f3f0808647b5392e8dcef18727d
RaghavKalani <raghavkalani2225@gmai.com>
2026-03-10 13:55:39 +0530

    Completed UC4: Character array based palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,6 @@
-public class PalindromeChecker {
+public class PalindromeCheckerApp {
 
     public static boolean isPalindrome(String text) {
-
         char[] chars = text.toCharArray();
 
         int start = 0;
@@ -19,14 +18,12 @@ public class PalindromeChecker {
     }
 
     public static void main(String[] args) {
-
         String input = "madam";
 
-        if(isPalindrome(input)){
-            System.out.println("Palindrome");
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a Palindrome");
         } else {
-            System.out.println("Not Palindrome");
+            System.out.println(input + " is NOT a Palindrome");
         }
-
     }
 }
